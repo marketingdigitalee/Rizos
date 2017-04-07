946fae6cc3dba087ae9b819042a58209fab0417f
psanchez <psanchez@elespectador.com>
2017-04-07 14:43:34 -0500

    cmabie la pieza de pasos y el color del contenedor

--- RizadorMVC/app/Vistas/secciones/instructivo.php
+++ RizadorMVC/app/Vistas/secciones/instructivo.php
@@ -1,5 +1,5 @@
 <h3 class="centrarTexto">CÓMO RESERVAR</h3>
       <p class="centrarTexto"> A continuación, encontrarás los pasos para hacer efectiva tu reserva</p>
       <div id="divInstructivo">
-        <img src="img/inst_reserva.jpg" alt="">
+        <img src="img/inst_reserva.png" alt="">
       </div>
