3c5af228796e8193acd01d1514736ddd86f283e0
psanchez <psanchez@elespectador.com>
2017-04-10 14:43:22 -0500

    puse botones de reserva y registro, banner y colores del formulario

--- RizadorMVC/app/Vistas/secciones/botones.php
+++ RizadorMVC/app/Vistas/secciones/botones.php
@@ -2,14 +2,14 @@
       <h3>Seleccione que desea hacer</h3>
         <div id="contenedorBotones1">
         	<a href="index.php?action=nuevo#ReservaYA" id="boton1">
-        		<label for="btnNuevaReserva">Quiero Reservar</label>
+        		<label for="btnNuevaReserva"></label>
         		<img id="btnNuevaReserva" src="img/btn_reserva.png" alt="">
         	</a>	
         </div>
         <div class="contenedorBotones2">
         	<a href="index.php?action=registrado#ReservaYA" id="boton2">
-        		<label for="btnRegistrado">Ya estoy registrado</label>
-        		<img id="btnRegistrado" src="img/btn_reserva.png" alt="">
+        		<label for="btnRegistrado"></label>
+        		<img id="btnRegistrado" src="img/btn_registrado.png" alt="">
         	</a>	
         </div>
       </div>
