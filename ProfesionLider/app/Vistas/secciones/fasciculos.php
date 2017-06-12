992cc735ac5a89c9b2f7a7012415bfba1525702e
LeandroRomero <lromero@elespectador.com>
2017-06-12 17:04:56 -0500

    Nuevas paginas de fasciculos

--- ProfesionLider/app/Vistas/secciones/fasciculos.php
+++ ProfesionLider/app/Vistas/secciones/fasciculos.php
@@ -8,11 +8,284 @@
         <h3><img alt="" src="img/pages/1.jpg" /></h3>
     </div>
     <div>
-        <h3><img alt="" src="img/pages/2.jpg" /></h3>
+        <h3><img alt="" src="img/pages/1a.jpg" /></h3>
     </div>
     <div>
         <h3>
-            <img alt="" src="img/pages/3.jpg" />
+            <img alt="" src="img/pages/1b.jpg" />
+            <section class="tableFasci_01">
+                <a href="#sky-form" class="modal-opener">
+                    <div class="descargaFasciculo"></div>
+                </a> 
+                <div class="contentFasiculo">
+                    <p class="nameFasci">Fasciculo de Prueba 1</p>
+                </div>
+                        
+
+            </section>
+            <section class="tableFasci_02">
+               <!--  <a href="#sky-form" class="modal-opener"> -->
+                    <div class="proximaDescarga"></div>
+                <!-- </a>  -->
+                <div class="contentFasiculo">
+                    <p class="nameFasci">Fasciculo de Prueba 2</p>
+                </div>
+                        
+
+            </section>
+            <section class="tableFasci_03">
+                <!--  <a href="#sky-form" class="modal-opener"> -->
+                    <div class="proximaDescarga"></div>
+                <!-- </a>  -->
+                <div class="contentFasiculo">
+                    <p class="nameFasci">Fasciculo de Prueba 3</p>
+                </div>
+                        
+
+            </section>
+  
+        </h3>
+    </div>
+        <div>
+        <h3><img alt="" src="img/pages/2a.jpg" /></h3>
+    </div>
+    <div>
+        <h3>
+            <img alt="" src="img/pages/2b.jpg" />
+            <section class="tableFasci_01">
+                <a href="#sky-form" class="modal-opener">
+                    <div class="descargaFasciculo"></div>
+                </a> 
+                <div class="contentFasiculo">
+                    <p class="nameFasci">Fasciculo de Prueba 1</p>
+                </div>
+                        
+
+            </section>
+            <section class="tableFasci_02">
+                <a href="#sky-form" class="modal-opener">
+                    <div class="descargaFasciculo"></div>
+                </a> 
+                <div class="contentFasiculo">
+                    <p class="nameFasci">Fasciculo de Prueba 2</p>
+                </div>
+                        
+
+            </section>
+            <section class="tableFasci_03">
+                <a href="#sky-form" class="modal-opener">
+                    <div class="descargaFasciculo"></div>
+                </a> 
+                <div class="contentFasiculo">
+                    <p class="nameFasci">Fasciculo de Prueba 3</p>
+                </div>
+                        
+
+            </section>
+  
+        </h3>
+    </div>
+        <div>
+        <h3><img alt="" src="img/pages/3a.jpg" /></h3>
+    </div>
+    <div>
+        <h3>
+            <img alt="" src="img/pages/3b.jpg" />
+            <section class="tableFasci_01">
+                <a href="#sky-form" class="modal-opener">
+                    <div class="descargaFasciculo"></div>
+                </a> 
+                <div class="contentFasiculo">
+                    <p class="nameFasci">Fasciculo de Prueba 1</p>
+                </div>
+                        
+
+            </section>
+            <section class="tableFasci_02">
+                <a href="#sky-form" class="modal-opener">
+                    <div class="descargaFasciculo"></div>
+                </a> 
+                <div class="contentFasiculo">
+                    <p class="nameFasci">Fasciculo de Prueba 2</p>
+                </div>
+                        
+
+            </section>
+            <section class="tableFasci_03">
+                <a href="#sky-form" class="modal-opener">
+                    <div class="descargaFasciculo"></div>
+                </a> 
+                <div class="contentFasiculo">
+                    <p class="nameFasci">Fasciculo de Prueba 3</p>
+                </div>
+                        
+
+            </section>
+  
+        </h3>
+    </div>
+        <div>
+        <h3><img alt="" src="img/pages/4a.jpg" /></h3>
+    </div>
+    <div>
+        <h3>
+            <img alt="" src="img/pages/4b.jpg" />
+            <section class="tableFasci_01">
+                <a href="#sky-form" class="modal-opener">
+                    <div class="descargaFasciculo"></div>
+                </a> 
+                <div class="contentFasiculo">
+                    <p class="nameFasci">Fasciculo de Prueba 1</p>
+                </div>
+                        
+
+            </section>
+            <section class="tableFasci_02">
+                <a href="#sky-form" class="modal-opener">
+                    <div class="descargaFasciculo"></div>
+                </a> 
+                <div class="contentFasiculo">
+                    <p class="nameFasci">Fasciculo de Prueba 2</p>
+                </div>
+                        
+
+            </section>
+            <section class="tableFasci_03">
+                <a href="#sky-form" class="modal-opener">
+                    <div class="descargaFasciculo"></div>
+                </a> 
+                <div class="contentFasiculo">
+                    <p class="nameFasci">Fasciculo de Prueba 3</p>
+                </div>
+                        
+
+            </section>
+  
+        </h3>
+    </div>
+        <div>
+        <h3><img alt="" src="img/pages/5a.jpg" /></h3>
+    </div>
+    <div>
+        <h3>
+            <img alt="" src="img/pages/5b.jpg" />
+            <section class="tableFasci_01">
+                <a href="#sky-form" class="modal-opener">
+                    <div class="descargaFasciculo"></div>
+                </a> 
+                <div class="contentFasiculo">
+                    <p class="nameFasci">Fasciculo de Prueba 1</p>
+                </div>
+                        
+
+            </section>
+            <section class="tableFasci_02">
+                <a href="#sky-form" class="modal-opener">
+                    <div class="descargaFasciculo"></div>
+                </a> 
+                <div class="contentFasiculo">
+                    <p class="nameFasci">Fasciculo de Prueba 2</p>
+                </div>
+                        
+
+            </section>
+            <section class="tableFasci_03">
+                <a href="#sky-form" class="modal-opener">
+                    <div class="descargaFasciculo"></div>
+                </a> 
+                <div class="contentFasiculo">
+                    <p class="nameFasci">Fasciculo de Prueba 3</p>
+                </div>
+                        
+
+            </section>
+  
+        </h3>
+    </div>
+        <div>
+        <h3><img alt="" src="img/pages/6a.jpg" /></h3>
+    </div>
+    <div>
+        <h3>
+            <img alt="" src="img/pages/6b.jpg" />
+            <section class="tableFasci_01">
+                <a href="#sky-form" class="modal-opener">
+                    <div class="descargaFasciculo"></div>
+                </a> 
+                <div class="contentFasiculo">
+                    <p class="nameFasci">Fasciculo de Prueba 1</p>
+                </div>
+                        
+
+            </section>
+            <section class="tableFasci_02">
+                <a href="#sky-form" class="modal-opener">
+                    <div class="proximaDescarga"></div>
+                </a> 
+                <div class="contentFasiculo">
+                    <p class="nameFasci">Fasciculo de Prueba 2</p>
+                </div>
+                        
+
+            </section>
+            <section class="tableFasci_03">
+                <a href="#sky-form" class="modal-opener">
+                    <div class="proximaDescarga"></div>
+                </a> 
+                <div class="contentFasiculo">
+                    <p class="nameFasci">Fasciculo de Prueba 3</p>
+                </div>
+                        
+
+            </section>
+  
+        </h3>
+    </div>
+        <div>
+        <h3><img alt="" src="img/pages/7a.jpg" /></h3>
+    </div>
+    <div>
+        <h3>
+            <img alt="" src="img/pages/7b.jpg" />
+            <section class="tableFasci_01">
+                <a href="#sky-form" class="modal-opener">
+                    <div class="descargaFasciculo"></div>
+                </a> 
+                <div class="contentFasiculo">
+                    <p class="nameFasci">Fasciculo de Prueba 1</p>
+                </div>
+                        
+
+            </section>
+            <section class="tableFasci_02">
+                <a href="#sky-form" class="modal-opener">
+                    <div class="descargaFasciculo"></div>
+                </a> 
+                <div class="contentFasiculo">
+                    <p class="nameFasci">Fasciculo de Prueba 2</p>
+                </div>
+                        
+
+            </section>
+            <section class="tableFasci_03">
+                <a href="#sky-form" class="modal-opener">
+                    <div class="descargaFasciculo"></div>
+                </a> 
+                <div class="contentFasiculo">
+                    <p class="nameFasci">Fasciculo de Prueba 3</p>
+                </div>
+                        
+
+            </section>
+  
+        </h3>
+    </div>
+        <div>
+        <h3><img alt="" src="img/pages/8a.jpg" /></h3>
+    </div>
+    <div>
+        <h3>
+            <img alt="" src="img/pages/8b.jpg" />
             <section class="tableFasci_01">
                 <a href="#sky-form" class="modal-opener">
                     <div class="descargaFasciculo"></div>
